d0f951bd8645d8d4fe65cc956a439e281870f50e
J_D_Nye <garwoodjdm@gmail.com>
2023-09-19 21:24:15 +1000

    added initial startup code

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+const User = model('user', userSchema)
+
+module.exports = User;
